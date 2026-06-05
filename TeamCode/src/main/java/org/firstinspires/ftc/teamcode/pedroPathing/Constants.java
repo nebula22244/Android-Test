40921a49eac43a31b33863587544d9166cc31c19
nebula22244 <nebula22244@mainsequencemt.org>
2026-06-04 19:20:41 -0600

    Initial Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,17 +3,54 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.ThreeWheelConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants().mass(5);   //TODO: please CHANGE MASS TO BE CORRECT, kg
+
+
+    public static MecanumConstants driveConstants = new MecanumConstants() //LATER PROBLEM oops
+            .maxPower(1)
+            .rightFrontMotorName("fr")
+            .rightRearMotorName("br")
+            .leftRearMotorName("bl")
+            .leftFrontMotorName("fl")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+
+
+    public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
+            .forwardTicksToInches(0.001978937)
+            .strafeTicksToInches(0.001978937)
+            .turnTicksToInches(0.001978937)
+            .leftPodY(-7.5)
+            .rightPodY(7.5)                                               //TODO fix this,  ticks to inches please
+            .strafePodX(4.65)
+            .leftEncoder_HardwareMapName("br")
+            .rightEncoder_HardwareMapName("bl")
+            .strafeEncoder_HardwareMapName("fl")
+            .leftEncoderDirection(Encoder.REVERSE)//other may need reversed if its being stupid and goes the other way
+            .rightEncoderDirection(Encoder.FORWARD)//same with this
+            .strafeEncoderDirection(Encoder.FORWARD)// and this
+            .forwardTicksToInches(.005)//this number is also very wrong do the tuning tests
+            .strafeTicksToInches(.005) //this is also just a place holder do the steps it says please
+            .turnTicksToInches(.005)//this is also a place holder guy
+;
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .threeWheelLocalizer(localizerConstants)
                 .build();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/MainTele.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp
+public class MainTele extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+        DcMotor fr = hardwareMap.get(DcMotor.class, "fr");
+        DcMotor br = hardwareMap.get(DcMotor.class, "br");
+        DcMotor fl = hardwareMap.get(DcMotor.class, "fl");
+        DcMotor bl = hardwareMap.get(DcMotor.class, "bl");
+
+        DcMotor shootl = hardwareMap.get(DcMotor.class, "shootl");
+        DcMotor shootr = hardwareMap.get(DcMotor.class, "shootr");
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double flPower = (-y + x + rx) / denominator;
+            double blPower = (-y - x + rx) / denominator;
+            double frPower = (y + x + rx) / denominator;
+            double brPower = (y - x + rx) / denominator;
+
+            fl.setPower(flPower);
+            bl.setPower(blPower);
+            fr.setPower(frPower);
+            br.setPower(brPower);
+
+
+
+            if (gamepad2.right_trigger>0.1){
+
+
+            }
+
+
+
+        }
+    }
+}
